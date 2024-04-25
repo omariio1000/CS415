7b1f7a0188126448f413d0fd74bc6ad39acbc7df
Omar Nassar <omariio1000@gmail.com>
2024-04-25 15:44:06 -0700

    working on lab 4

--- Labs/lab4/sum-omp.c
+++ Labs/lab4/sum-omp.c
@@ -10,26 +10,44 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
+#include <omp.h>
+#include <sched.h>
 
 long compute(long i) { return sqrt(i*i); }
 
 int main(int argc, char **argv) {
   long N = 0;
+  int threads = 1;
   struct timeval start, end;
 
-  if (argc == 1)
+  if (argc == 1) {
     printf("Usage: ./sum N");
-  else {
-    N = strtol(argv[1], NULL, 10);
-  }  
+    return EXIT_FAILURE;
+  }
+
+  else if (argc == 3) 
+    threads = strtod(argv[2], NULL);
+
+  N = strtol(argv[1], NULL, 10);
+  omp_set_num_threads(threads);
 
   long sum = 0;
   gettimeofday(&start, NULL);
-  for (int i = 0; i < N; i++)
+
+
+#pragma omp parallel for reduction(+:sum)
+  for (int i = 0; i < N; i++) {
     sum += compute(i);
+#ifdef DEBUG
+  printf("Thread id: %d\n", omp_get_thread_num());
+#endif
+  }
+
   gettimeofday(&end, NULL);
 
 
   printf("Sum of %ld sqrt(i*i) is  %ld\n", N, sum);
-  printf("Seq-version elapsed time:  %ld us\n", end.tv_usec - start.tv_usec);
+  long elapsed_time = (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec;
+  printf("OMP-version (%d threads) elapsed time:  %ld us\n", threads, elapsed_time);
+
 }  
