aa02b4abe7e88ea3b42ec2278929b1d4813b3523
Omar Nassar <omariio1000@gmail.com>
2024-05-01 13:01:26 -0700

    part 1 done

--- Assignments/assign2/prime-par1.cpp
+++ Assignments/assign2/prime-par1.cpp
@@ -6,10 +6,56 @@
 //
 // Usage:
 //   linux> ./prime N
-//
+// 0
 #include <iostream>
 #include <cmath>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <vector>
 using namespace std;
+
+mutex totalPrimesMutex;
+int totalPrimes = 0;
+
+void worker(int start, int end, int k, int P, const vector<int> &sieve)
+{
+    int range = end - start + 1;
+    int remain = range % P;
+    int startIdx = (range / P) * k + std::min(k, remain) + start;
+    int endIdx = startIdx + (range / P) - 1 + (k < remain ? 1 : 0);
+
+    int primesFound = 0;
+
+    bool candidate[endIdx - startIdx + 1];
+    bool sieveArr[start];
+
+    for (int i = 0; i < max(endIdx - startIdx + 1, start); i++)
+    {
+        if (i < range)
+            candidate[i] = true;
+        if (i < start)
+            sieveArr[i] = false;
+    }
+
+    for (int prime : sieve)
+        sieveArr[prime] = true;
+
+    for (int i = 2; i <= start - 1; i++)
+        if (sieveArr[i])
+            for (int j = i + i; j <= endIdx; j += i)
+                if (j >= startIdx)
+                    candidate[j - startIdx] = false;
+
+    for (int i = 0; i < endIdx - startIdx + 1; i++)
+        if (candidate[i])
+            primesFound++;
+
+    printf("Worker[%d] found %d primes in [%d..%d]\n", k, primesFound, startIdx, endIdx);
+
+    lock_guard<mutex> guard(totalPrimesMutex);
+    totalPrimes += primesFound;
+}
 
 int main(int argc, char **argv)
 {
@@ -35,7 +81,38 @@ int main(int argc, char **argv)
         }
     }
 
+    printf("prime-par1 (%d threads) over [%d..%d] ...\n", P, 2, N);
+    auto start = chrono::steady_clock::now();
     int sqrtN = sqrt(N);
-    bool candidate[N+1];
-    int sieve[sqrtN];
+    bool candidate[sqrtN + 1];
+    vector<int> sieve;
+
+    for (int i = 2; i <= sqrtN; i++)
+        candidate[i] = true;
+
+    for (int i = 2; i <= sqrt(N); i++)
+        if (candidate[i])
+        {
+            sieve.push_back(i);
+            for (int j = i + i; j <= sqrtN; j += i)
+                candidate[j] = false;
+        }
+
+    for (int i = 2; i <= sqrtN; i++)
+        if (candidate[i])
+            totalPrimes++;
+
+    printf("Master found %d primes in [%d..%d]\n", totalPrimes, 2, sqrtN);
+
+    thread workerTh[P];
+    for (int i = 0; i < P; i++)
+        workerTh[i] = thread(worker, sqrtN + 1, N, i, P, cref(sieve));
+
+    for (int i = 0; i < P; i++)
+        workerTh[i].join();
+
+    auto end = chrono::steady_clock::now();
+    auto duration = chrono::duration<double, std::milli>(end - start).count();
+
+    cout << "prime-par1 (N=" << N << ") found " << totalPrimes << " primes in " << duration << "ms\n";
 }
